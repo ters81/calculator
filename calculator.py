5b822c84de2067bbfcca87d4de92006fa8108b93
Serhii Tereshchuk <57225875+ters81@users.noreply.github.com>
2020-12-15 15:57:44 +0300

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,37 +1,83 @@
 import tkinter as tk
 
-
 # Функция ввода цифр в поле ввода
 
+x = True
+
+
 def add_digit(digit):
+    global x
     value = entry_field.get()
-    if digit == '.' and value.count('.') == 1:
-        entry_field.delete(0, tk.END)
-        entry_field.insert(0, value)
+    if x:
+        if digit == '.' and value.count('.') == 1:
+            entry_field.delete(0, tk.END)
+            entry_field.insert(0, value)
+        else:
+            if value[0] == '0' and digit == '.':
+                value = value
+            elif value[0] == '0' and len(value) == 1:
+                value = value[:-1]
+            entry_field.delete(0, tk.END)
+            entry_field.insert(0, value + str(digit))
     else:
-        if value[0] == '0' and digit == '.':
-            value = value
-        elif value[0] == '0' and len(value) == 1:
-            value = value[:-1]
         entry_field.delete(0, tk.END)
-        entry_field.insert(0, value + str(digit))
+        entry_field.insert(0, str(digit))
+        x = True
+
 
 def add_operation(operation):
+    global x
     value = entry_field.get()
-    if value[-1] in '÷x+-':
-        value = value[:-1]
-    entry_field.delete(0, tk.END)
-    entry_field.insert(0, value + operation)
+    if x:
+        if value[-1] in '÷x+-':
+            value = value[:-1]
+        elif '/' in value or '*' in value or '+' in value or '-' in value:
+            calculate()
+            value = entry_field.get()
+
+        entry_field.delete(0, tk.END)
+        entry_field.insert(0, value + operation)
+    else:
+        entry_field.delete(0, tk.END)
+        entry_field.insert(0, value + operation)
+        x = True
 
 
 def make_digit_button(digit):
     return tk.Button(text=digit, bg='#f1f3f4', font='arial', bd=2, command=lambda: add_digit(digit))
 
+
 def make_operation_button(operation):
-    return tk.Button(text=operation, bg='#dadce0', font='arial', fg='blue', bd=2, command=lambda: add_operation(operation))
+    return tk.Button(text=operation, bg='#dadce0', font='arial', fg='blue', bd=2,
+                     command=lambda: add_operation(operation))
+
+
+def calculate():
+    value = entry_field.get()
+    if value[-1] in '/*+-':
+        value = value + value[:-1]
+
+    entry_field.delete(0, tk.END)
+    y = str(eval(value))
+    if y[-2::] == '.0':
+        y = y.rstrip('0').rstrip('.')
+    entry_field.insert(0, y)
+
+
+def calculate_button():
+    calculate()
+    global x
+    x = False
+
+
+def clear_entry_field():
+    entry_field.delete(0, tk.END)
+    entry_field.insert(0, '0')
+
 
 def make_calc_button(operation):
-    return tk.Button(text=operation, bg='#4285f4', font='arial', fg='white', bd=2, command=lambda: add_digit(operation))
+    return tk.Button(text=operation, bg='#4285f4', font='arial', fg='white', bd=2, command=lambda: calculate_button())
+
 
 win = tk.Tk()
 
@@ -67,9 +113,11 @@ entry_field.insert(0, '0')
 entry_field.grid(row=0, column=0, columnspan=3, stick='we', pady=5, padx=5)
 
 # Кнопки с цифрами
-tk.Button(text='AC', bg='#dadce0', font='arial', bd=2).grid(row=0, column=4, stick='wens', padx=5, pady=5)
-make_operation_button('÷').grid(row=1, column=4, stick='wens', padx=5, pady=5)
-make_operation_button('×').grid(row=2, column=4, stick='wens', padx=5, pady=5)
+tk.Button(text='AC', bg='#dadce0', font='arial', bd=2, command=lambda: clear_entry_field()).grid(row=0, column=4,
+                                                                                                 stick='wens', padx=5,
+                                                                                                 pady=5)
+make_operation_button('/').grid(row=1, column=4, stick='wens', padx=5, pady=5)
+make_operation_button('*').grid(row=2, column=4, stick='wens', padx=5, pady=5)
 make_operation_button('+').grid(row=3, column=4, stick='wens', padx=5, pady=5)
 make_operation_button('-').grid(row=4, column=4, stick='wens', padx=5, pady=5)
 
@@ -88,13 +136,6 @@ make_digit_button('6').grid(row=2, column=2, stick='wens', padx=5, pady=5)
 make_digit_button('7').grid(row=3, column=0, stick='wens', padx=5, pady=5)
 make_digit_button('8').grid(row=3, column=1, stick='wens', padx=5, pady=5)
 make_digit_button('9').grid(row=3, column=2, stick='wens', padx=5, pady=5)
-
-# x = 1
-# for i in range(1, 4):
-#     for j in range(3):
-#         tk.Button(text=f'{x}', bg='#f1f3f4', font='arial', bd=2, command=lambda: add_digit()).grid(row=i, column=j, stick='wens', padx=5, pady=5)
-#         x += 1
-
 
 win.grid_columnconfigure(0, weight=1)
 win.grid_columnconfigure(1, weight=1)

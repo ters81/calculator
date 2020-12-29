a782f47d6539d556955543162e27bc375fc17bfe
Serhii Tereshchuk <57225875+ters81@users.noreply.github.com>
2020-12-29 17:18:55 +0300

    Add files via upload

--- calculator-v2.py
+++ calculator-v2.py
@@ -4,16 +4,31 @@ from tkinter import messagebox
 
 # Функция ввода цифр в поле ввода
 
+x = True
+
+# def decor(func):
+#     # Блокировка окон ввода/вывода (избегание ввода букв в окна)
+#     def dec(*args, **kwargs):
+#         entry_field['state'] = tk.NORMAL
+#         func(*args, **kwargs)
+#         entry_field['state'] = tk.DISABLED
+#     return dec
+
+
 def add_digit(digit):
+    entry_field['state'] = tk.NORMAL
+    global x
     value_1 = entry_field.get('1.0', '1.18')
     value_2 = entry_field.get('2.0', '2.2')
     value_3 = entry_field.get('3.0', '3.18')
 
-    if value_2:
+    if value_2 and x:
         if len(value_3) == 18:
+            # Вводимое второе число не больше 18 символов
             pass
 
         elif ')' in value_3:
+            # После закрытия скобки во втором числе, ввод прекращается
             pass
 
         elif digit == '.' and len(value_3) == 0:
@@ -28,11 +43,33 @@ def add_digit(digit):
             # Чтобы вводилась только одна точка при вводе числа
             pass
 
+        elif value_3 == '0' and digit != '.':
+            #  Чтобы небыло возможности ввести во втором числе например '085' ... замена на '85'
+            entry_field.delete('3.0', '3.2')
+            entry_field.insert('3.0', digit)
+
+        elif value_3 == '(0' and digit != '.':
+            #  Чтобы небыло возможности ввести во втором числе например '(085...' ... замена на '(85...'
+            entry_field.delete('3.0', '3.3')
+            entry_field.insert('3.0', '(' + digit)
+
+        elif value_3 == '(-0' and digit != '.':
+            #  Чтобы небыло возможности ввести во втором числе например '(-085...' ... замена на '(-85...'
+            entry_field.delete('3.0', '3.4')
+            entry_field.insert('3.0', '(-' + digit)
+
+        elif digit == '.' and (value_3 == '(' or value_3 == '(-'):
+            # Если ввести точку во втором числе при открытой скобке или скобке с минусом, то подставиться "0."
+            result = entry_field.get('3.0', '3.2')
+            entry_field.delete('3.0', '3.2')
+            entry_field.insert('3.0', result + '0.')
+
         else:
             # Ввод цифр
             entry_field.insert(tk.E, digit)
-    else:
+    elif x:
         if len(value_1) == 18:
+            # Вводимое второе число не больше 18 символов
             pass
 
         elif digit == '.' and len(value_1) == 0:
@@ -52,24 +89,50 @@ def add_digit(digit):
             # Чтобы вводилась только одна точка при вводе числа
             pass
 
+        elif value_1 == '0' and digit != '.':
+            #  Чтобы небыло возможности ввести в первом числе например '085' ... замена на '85'
+            entry_field.delete('1.0', '1.2')
+            entry_field.insert('1.0', digit)
+
         else:
             # Ввод цифр
             entry_field.insert(tk.E, digit)
+    else:
+        entry_field.delete('1.0', tk.END)
+        if digit == '.':
+            entry_field.insert('1.0', '0.')
+        else:
+            entry_field.insert('1.0', digit)
+        entry_field_result['state'] = tk.NORMAL
+        entry_field_result.delete(0, tk.END)
+        entry_field_result.insert(0, '0')
+        entry_field_result['state'] = tk.DISABLED
+        x = True
+    entry_field['state'] = tk.DISABLED
 
 
 def add_operation(operation):
+    entry_field['state'] = tk.NORMAL
+    global x
     value_1 = entry_field.get('1.0', '1.18')
     value_3 = entry_field.get('3.0', '3.18')
 
-    if (operation == '-' and len(value_1) == 0) or (operation == '-' and value_1 == '-'):
+    if value_3 and value_3 != '(':
+        calculate()
+        result = entry_field_result.get()
+        entry_field.delete('1.0', tk.END)
+        entry_field.insert('1.0', result + '\n')
+        entry_field.insert('2.0', ' ' + operation + '\n')
+
+    elif (operation == '-' and len(value_1) == 0) or (operation == '-' and value_1 == '-'):
         entry_field.delete('1.0', tk.END)
         entry_field.insert('1.0', '-')
 
     elif (operation in '/*+' and value_1 == '-') or (operation in '/*+' and len(value_1) == 0) or value_3.count('-') == 1:
         pass
 
-    # Возможность ввести отрицательное число после скобки во 2-м вводимом значении
     elif value_3 and value_3[0] == '(' and operation == '-':
+        # Возможность ввести отрицательное число после скобки во 2-м вводимом числе
         entry_field.insert('3.1', '-')
 
     else:
@@ -77,18 +140,25 @@ def add_operation(operation):
         entry_field.insert('1.0', value_1 + '\n')
         entry_field.delete('2.0', '2.18')
         entry_field.insert('2.0', ' ' + operation + '\n')
+    x = True
+    entry_field['state'] = tk.DISABLED
 
 
 def add_brackets(bracket):
+    entry_field['state'] = tk.NORMAL
     value_2 = entry_field.get('2.0', '2.2')
     value_3 = entry_field.get('3.0', '3.18')
 
     if value_2 and len(value_3) == 0 and bracket == '(':
         entry_field.insert('3.0', bracket)
+
     elif value_2 and bracket == ')' and '(' in value_3 and len(value_3) > 1 and value_3.count(')') < 1:
         entry_field.insert(tk.END, bracket)
+    entry_field['state'] = tk.DISABLED
+
 
 def calculate():
+    entry_field_result['state'] = tk.NORMAL
     value_1 = entry_field.get('1.0', '1.18')
     value_2 = entry_field.get('2.0', '2.2')
     value_3 = entry_field.get('3.0', '3.18')
@@ -106,15 +176,23 @@ def calculate():
     except ZeroDivisionError:
         messagebox.showinfo('Внимание', 'Нельзя делить на 0!')
         entry_field.insert(0, 0)
+    entry_field_result['state'] = tk.DISABLED
+
 
 def clear_entry_field():
+    entry_field['state'] = tk.NORMAL
+    entry_field_result['state'] = tk.NORMAL
     entry_field.delete('1.0', tk.END)
     entry_field_result.delete(0, tk.END)
     entry_field_result.insert(0, '0')
+    entry_field['state'] = tk.DISABLED
+    entry_field_result['state'] = tk.DISABLED
 
 
 def calculate_button():
     calculate()
+    global x
+    x = False
 
 
 def make_digit_button(digit):
@@ -125,20 +203,33 @@ def make_operation_button(operation):
     return tk.Button(text=operation, bg='#dadce0', font='arial', fg='blue', bd=2,
                      command=lambda: add_operation(operation))
 
+
 def make_brackets(bracket):
     return tk.Button(text=bracket, bg='#dadce0', font='arial', fg='blue', bd=2,
                      command=lambda: add_brackets(bracket))
+
 
 def make_calc_button(operation):
     return tk.Button(text=operation, bg='#4285f4', font='arial', fg='white', bd=2, command=lambda: calculate_button())
 
 
+def press_key(event):
+    if event.char.isdigit() or event.char == '.':
+        add_digit(event.char)
+    elif event.char in '/*+-':
+        add_operation(event.char)
+    elif event.char == '\r':
+        calculate_button()
+    elif event.char == '\x1b':
+        clear_entry_field()
+    # print(event)
+
+
 win = tk.Tk()
 
 # Размеры окна
-# win.geometry("240x260")
-win.minsize(240, 300)
-win.maxsize(240, 300)
+win.geometry("240x300")
+win.resizable(False, False)
 
 # Размещение окна по центру экрана
 win.update_idletasks()
@@ -159,19 +250,22 @@ win.geometry(f'+{w}+{h}')
 # Внешний вид окна
 win.config(bg='#ffffff', relief='raised')
 # Название окна
-win.title('Калькулятор')
+win.title('Калькулятор v2')
+
+win.bind('<Key>', press_key)
 
 # Поле ввода
-entry_field = tk.Text(master=win, font='arial', width=15, height=5, borderwidth=5)
-# entry_field.insert('1.0', '0')
-# entry_field['state'] = tk.DISABLED
+entry_field = tk.Text(master=win, font='arial', width=15, height=3, borderwidth=5)
 entry_field.grid(row=0, column=0, columnspan=3, stick='we', pady=5, padx=5)
+entry_field['state'] = tk.DISABLED
+
 
 # Поле вывода результата
 entry_field_result = tk.Entry(master=win, font='arial', width=15, borderwidth=5, justify=tk.RIGHT)
 entry_field_result.insert(0, '0')
-# entry_field_result['state'] = tk.DISABLED
 entry_field_result.grid(row=1, column=0, columnspan=3, stick='we', pady=5, padx=5)
+entry_field_result['state'] = tk.DISABLED
+
 
 # Кнопки с цифрами
 tk.Button(text='AC', bg='#dadce0', font='arial', bd=2, command=lambda: clear_entry_field()).grid(row=0, column=4,
@@ -183,7 +277,7 @@ tk.Button(text='AC', bg='#dadce0', font='arial', bd=2, command=lambda: clear_ent
 make_brackets('(').grid(row=1, column=4, stick='wens', padx=2, pady=5)
 make_brackets(')').grid(row=1, column=5, stick='wens', padx=5, pady=5)
 
-# Символы операций
+# Символы цифр и операций
 make_operation_button('/').grid(row=2, column=4, columnspan=2, stick='wens', padx=5, pady=5)
 make_operation_button('*').grid(row=3, column=4, columnspan=2, stick='wens', padx=5, pady=5)
 make_operation_button('+').grid(row=4, column=4, columnspan=2, stick='wens', padx=5, pady=5)
